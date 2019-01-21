d04525f511baf329a6a38ba3b33e2bcf1eb0e4fc
Rob <ListeningTalker@hotmail.com>
2019-01-21 09:31:35 -0500

    Add project files.

---
+++ CPAT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CPAT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
