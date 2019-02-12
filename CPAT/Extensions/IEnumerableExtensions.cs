e7f21ac9df61448ac4cc16911deb5ac1958a7b82
Rob <ListeningTalker@hotmail.com>
2019-02-11 22:20:22 -0500

    Updates to Student Plans

---
+++ CPAT/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace CPAT.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this IEnumerable<T> items, int selectedValue)
+        {
+            return from item in items
+                   select new SelectListItem
+                   {
+                       Text = item.GetPropertyValue("Name"),
+                       Value = item.GetPropertyValue("Id"),
+                       Selected = item.GetPropertyValue("Id").Equals(selectedValue.ToString())
+                   };
+        }
+    }
+}
